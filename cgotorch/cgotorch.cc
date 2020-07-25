05acf083cee67a2cf7639864abc8990da6a2d920
qijun <qijun1994@hotmail.com>
2020-07-25 21:01:34 +0800

    keep std::move

--- cgotorch/cgotorch.cc
+++ cgotorch/cgotorch.cc
@@ -7,18 +7,18 @@
 Tensor RandN(int rows, int cols, int require_grad) {
   at::Tensor t = torch::randn({rows,cols},
 			      at::TensorOptions().requires_grad(require_grad));
-  return new at::Tensor(t);
+  return new at::Tensor(std::move(t));
 }
 
 Tensor MM(Tensor a, Tensor b) {
   at::Tensor c = at::mm(*static_cast<at::Tensor*>(a),
 			*static_cast<at::Tensor*>(b));
-  return new at::Tensor(c);
+  return new at::Tensor(std::move(c));
 }
 
 Tensor Sum(Tensor a) {
   at::Tensor r = static_cast<at::Tensor*>(a)->sum();
-  return new at::Tensor(r);
+  return new at::Tensor(std::move(r));
 }
 
 void Backward(Tensor a) {
@@ -27,7 +27,7 @@ void Backward(Tensor a) {
 
 Tensor Grad(Tensor a) {
   at::Tensor r = static_cast<at::Tensor*>(a)->grad();
-  return new at::Tensor(r);
+  return new at::Tensor(std::move(r));
 }
 
 void PrintTensor(Tensor a) {
