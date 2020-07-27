832af360d50d03ffe8584a5acb9e3e88f654d61d
Yancey1989 <yancey1989@gmail.com>
2020-07-27 19:43:36 +0800

    update

--- 01-backward.go
+++ 01-backward.go
@@ -7,26 +7,31 @@ package main
 import "C"
 import "fmt"
 
+// Tensor wrappers a pointer to C.Tensor
 type Tensor struct {
 	T C.Tensor
 }
 
-func RandN(rows, cols int, require_grad bool) Tensor {
+// RandN returns a tensor filled with random number
+func RandN(rows, cols int, requireGrad bool) Tensor {
 	rg := 0
-	if require_grad {
+	if requireGrad {
 		rg = 1
 	}
 	return Tensor{C.RandN(C.int(rows), C.int(cols), C.int(rg))}
 }
 
+// MM multiplies each element of the input two tensors
 func MM(a, b Tensor) Tensor {
 	return Tensor{C.MM(a.T, b.T)}
 }
 
+// Sum returns the sum of all elements in the input tensor
 func Sum(a Tensor) Tensor {
 	return Tensor{C.Sum(a.T)}
 }
 
+// String returns the Tensor as a string
 func (a Tensor) String() string {
 	s := C.Tensor_String(a.T)
 	r := C.GoString(s)
@@ -34,14 +39,17 @@ func (a Tensor) String() string {
 	return r
 }
 
+// Print the tensor
 func (a Tensor) Print() {
 	C.Tensor_Print(a.T)
 }
 
+// Backward computes the gradient of current tensor
 func (a Tensor) Backward() {
 	C.Tensor_Backward(a.T)
 }
 
+// Grad returns a reference of the gradient
 func (a Tensor) Grad() Tensor {
 	return Tensor{C.Tensor_Grad(a.T)}
 }
