db0a4ee5a2385116bf6daddcdda9915d6b7f204d
Yi Wang <yi.wang.2005@gmail.com>
2020-07-27 16:57:52 -0700

    Update

--- .scripts/install-ci-tools.sh
+++ .scripts/install-ci-tools.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+V=$(go version | cut -f 3 -d ' ' | sed 's/go//')
+if [[ "$V" < "1.13" ]]; then
+    curl -Lso go.tar.gz https://golang.org/dl/go1.14.6.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xzf go.tar.gz
+    export PATH=/usr/local/go/bin:$PATH
+fi
+
 go version
 go env -w GO111MODULE=on
 go get golang.org/x/lint/golint
