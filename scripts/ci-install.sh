a8165b411daba6849a65a4ad9e8d47905c43a75e
Yi Wang <yi.wang.2005@gmail.com>
2020-07-28 13:03:31 -0700

    Update ci-install.sh

--- scripts/ci-install.sh
+++ scripts/ci-install.sh
@@ -2,13 +2,5 @@
 
 set -ex
 
-go env -w GO111MODULE=on
-go get \
-   golang.org/x/lint/golint
-
-echo "$GOPATH"
-echo "$GOROOT"
-
-ls -lt "$GOPATH"/bin
-
-cp "$GOPATH"/bin/* /usr/local/bin/
+go get golang.org/x/lint/golint
+sudo cp "$GOPATH"/bin/* /usr/local/bin/
