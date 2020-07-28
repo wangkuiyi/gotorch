48a43f1bab406fadce1257fdc34169803498c9db
Yi Wang <yi.wang.2005@gmail.com>
2020-07-28 12:32:41 -0700

    Update

--- .scripts/install-ci-tools.sh
+++ .scripts/install-ci-tools.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-EV="1.14.6"
-V=$(go version | cut -f 3 -d ' ' | sed 's/go//')
-if [[ "$V" < "EV" ]]; then
-    O=$(uname | tr [:upper:] [:lower:])
-    curl -Lso go.tar.gz https://golang.org/dl/go"$EV"."$O"-amd64.tar.gz
-    sudo tar -C /usr/local -xzf go.tar.gz
-    sudo mv /usr/local/go/bin/* /usr/local/bin/
-fi
-
 go version
 go get golang.org/x/lint/golint
 
