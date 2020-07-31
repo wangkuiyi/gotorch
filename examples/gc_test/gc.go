cff89e568ac1b5e30a397ec71ff730aa4b43831e
Yi Wang <yi.wang.2005@gmail.com>
2020-07-30 20:13:18 -0700

    Update

--- examples/gc_test/gc.go
+++ examples/gc_test/gc.go
@@ -7,10 +7,14 @@ import (
 	torch "github.com/wangkuiyi/gotorch"
 )
 
-func main() {
+func f() {
 	b := torch.RandN(4, 1, true)
 	runtime.SetFinalizer(&b, func(f *torch.Tensor) {
 		f.Close()
 		fmt.Println("Closed tensor")
 	})
+}
+
+func main() {
+	f()
 }
