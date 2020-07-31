f360cbbd903777af668f03b08e10adb569370d34
Yi Wang <yi.wang.2005@gmail.com>
2020-07-30 20:38:58 -0700

    Update

--- examples/gc_test/gc.go
+++ examples/gc_test/gc.go
@@ -8,11 +8,14 @@ import (
 )
 
 func f() {
+	fmt.Println("Entering f()")
 	b := torch.RandN(4, 1, true)
 	fmt.Println(b)
+	fmt.Println("Exiting f()")
 }
 
 func main() {
 	f()
+	fmt.Println("Calling runtime.GC()")
 	runtime.GC()
 }
