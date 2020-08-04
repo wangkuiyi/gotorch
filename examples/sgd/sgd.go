d2448c82196986b263edc2d8f62cdafdc1de0f3a
qijun <qijun1994@hotmail.com>
2020-08-04 19:04:28 +0800

    add Linear module

--- examples/sgd/sgd.go
+++ examples/sgd/sgd.go
@@ -4,15 +4,39 @@ import (
 	torch "github.com/wangkuiyi/gotorch"
 )
 
+// MyNet struct
+type myNet struct {
+	torch.Model
+	l1, l2 torch.Module
+}
+
+// MyNet returns a MyNet instance
+func MyNet() torch.Module {
+	n := &myNet{
+		l1: torch.Linear(100, 200, false),
+		l2: torch.Linear(200, 10, false),
+	}
+	n.RegisterModule("l1", n.l1)
+	n.RegisterModule("l2", n.l2)
+	return n
+}
+
+// Forward executes the calculation
+func (n *myNet) Forward(x torch.Tensor) torch.Tensor {
+	x = n.l1.Forward(x)
+	x = n.l2.Forward(x)
+	return x
+}
+
 func main() {
-	a := torch.RandN(100, 10, true)
+	net := MyNet()
 	opt := torch.NewSGDOpt(0.1, 0, 0, 0, false)
-	opt.AddParameters([]torch.Tensor{a})
+	opt.AddParameters(torch.GetParameters(net))
 
 	for i := 0; i < 100; i++ {
 		torch.GC()
-		b := torch.RandN(10, 100, false)
-		pre := torch.MM(b, a)
+		data := torch.RandN(32, 100, false)
+		pre := net.Forward(data)
 		loss := torch.Sum(pre)
 
 		opt.ZeroGrad()
