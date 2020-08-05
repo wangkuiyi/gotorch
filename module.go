5f87f31c03a23c5617c0ecbd3e77a71743d71c32
Yi Wang <yi.wang.2005@gmail.com>
2020-08-04 18:20:59 -0700

    Remove functions RegisterXxx (#52)
    * Remove RegisterXXX
    
    * Try reflect
    
    * Rewrite GetNamedParameter
    
    * Update .gitignore
    
    * Fatal if Tensor or Module-typed fields are not exported
    
    * Update sgd_test.go to print named params and buffers
    
    * Add comments
    
    * Fix bug
    
    * Shorten code

--- module.go
+++ module.go
@@ -5,46 +5,17 @@ package gotorch
 // #cgo LDFLAGS: -L ${SRCDIR}/cgotorch/libtorch/lib -Wl,-rpath ${SRCDIR}/cgotorch/libtorch/lib -lc10 -ltorch -ltorch_cpu
 // #include "cgotorch.h"
 import "C"
-import "reflect"
+import (
+	"log"
+	"reflect"
+)
 
 // Module interface
 type Module interface {
 	Forward(x Tensor) Tensor
 }
 
-// Model struct
-type Model struct {
-	parameters map[string]Tensor
-	buffers    map[string]Tensor
-	modules    map[string]Module
-}
-
-// RegisterBuffer registers a buffer to the model
-func (m *Model) RegisterBuffer(s string, t Tensor) {
-	if m.buffers == nil {
-		m.buffers = make(map[string]Tensor)
-	}
-	m.buffers[s] = t
-}
-
-// RegisterParameter registers a parameter to the model
-func (m *Model) RegisterParameter(s string, p Tensor) {
-	if m.parameters == nil {
-		m.parameters = make(map[string]Tensor)
-	}
-	m.parameters[s] = p
-}
-
-// RegisterModule registers a module to the model
-func (m *Model) RegisterModule(s string, module Module) {
-	if m.modules == nil {
-		m.modules = make(map[string]Module)
-	}
-	m.modules[s] = module
-}
-
 type linear struct {
-	Model
 	InFeatures  int
 	OutFeatures int
 	Weight      Tensor
@@ -58,10 +29,8 @@ func Linear(in int, out int, bias bool) Module {
 		OutFeatures: out,
 	}
 	l.Weight = RandN(in, out, true)
-	l.RegisterParameter("Weight", l.Weight)
 	if bias {
 		l.Bias = RandN(out, 1, true)
-		l.RegisterParameter("Bias", l.Bias)
 	}
 	return l
 }
@@ -71,35 +40,63 @@ func (l *linear) Forward(x Tensor) Tensor {
 	return MM(x, l.Weight)
 }
 
-// GetChildrenModules returns children modules recursively
-func GetChildrenModules(m Module) map[string]Module {
-	result := make(map[string]Module)
-	model := reflect.ValueOf(m).Elem().Field(0).Interface().(Model)
-	for name, cm := range model.modules {
-		result[name] = cm
-		res := GetChildrenModules(cm)
-		for k, v := range res {
-			result[k] = v
-		}
-	}
-	return result
+// GetNamedParameters returns parameters in the module recursively.
+func GetNamedParameters(m Module) map[string]Tensor {
+	r := make(map[string]Tensor)
+	getNamedNonNilTensors(m, reflect.TypeOf(m).Elem().Name(), true, false, r)
+	return r
 }
 
-// GetNamedParameters returns named parameters
-func GetNamedParameters(m Module) map[string]Tensor {
-	result := make(map[string]Tensor)
-	model := reflect.ValueOf(m).Elem().Field(0).Interface().(Model)
-	for k, v := range model.parameters {
-		result[k] = v
-	}
-	for moduleName, v := range GetChildrenModules(m) {
-		cmodel := reflect.ValueOf(v).Elem().Field(0).Interface().(Model)
-		for paramName, cv := range cmodel.parameters {
-			name := moduleName + "_" + paramName
-			result[name] = cv
+func getNamedNonNilTensors(m Module, prefix string, param, buffer bool, r map[string]Tensor) {
+	moduleType := reflect.TypeOf((*Module)(nil)).Elem()
+
+	sv := reflect.ValueOf(m).Elem() // Elem gets what the pointer points to.
+	for i := 0; i < sv.NumField(); i++ {
+		f := sv.Type().Field(i)
+		v := sv.Field(i)
+
+		if f.Type.Implements(moduleType) {
+			if !v.CanInterface() {
+				log.Fatalf("GoTorch requires exporting Module field %s.%s",
+					sv.Type().Name(), f.Name)
+			}
+			getNamedNonNilTensors(v.Interface().(Module),
+				prefix+"."+f.Name, param, buffer, r)
+		} else {
+			recordNonNilTensor(f, v, prefix, r, param, buffer)
 		}
 	}
-	return result
+}
+
+// If field f is a parameter or buffer field and the value v is not a nil
+// tensor, insert v into map r with key is prefix+"."+f.Name.
+func recordNonNilTensor(f reflect.StructField, v reflect.Value,
+	prefix string, r map[string]Tensor, param, buffer bool) {
+
+	tensorType := reflect.TypeOf((*Tensor)(nil)).Elem()
+	if f.Type != tensorType {
+		return // Either parameter or buffer is of type Tensor.
+	}
+
+	tag := f.Tag.Get("gotorch")
+	if !buffer && tag == "buffer" {
+		return // Don't wants a buffer but this field is one.
+	}
+	if !param && (tag == "param" || tag == "") {
+		return // Don't wants a parameter but this field is one.
+	}
+
+	if !v.CanInterface() {
+		log.Fatalf("GoTorch requires exporting Tensor field %s.%s",
+			v.Type().Name(), f.Name)
+	}
+
+	fv := v.Interface().(Tensor)
+	if fv.T == nil {
+		return // Don't record nil Tensor
+	}
+
+	r[prefix+"."+f.Name] = fv
 }
 
 // GetParameters returns parameters
@@ -114,8 +111,9 @@ func GetParameters(m Module) []Tensor {
 
 // CloseModule closes the module
 func CloseModule(m Module) {
-	params := GetParameters(m)
-	for _, p := range params {
-		p.Close()
+	r := make(map[string]Tensor)
+	getNamedNonNilTensors(m, reflect.TypeOf(m).Elem().Name(), true, true, r)
+	for _, t := range r {
+		t.Close()
 	}
 }
