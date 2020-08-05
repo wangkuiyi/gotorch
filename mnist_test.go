0d1d856436c1bdb5dba386bebcdcb4f8893fecc9
Yi Wang <yi.wang.2005@gmail.com>
2020-08-05 15:43:56 -0700

    Download MNIST dataset if not exists

--- mnist_test.go
+++ mnist_test.go
@@ -1,11 +1,23 @@
 package gotorch_test
 
 import (
+	"compress/gzip"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/user"
+	"path"
+
 	torch "github.com/wangkuiyi/gotorch"
 )
 
-func NotWorkingExampleMNIST() {
-	dataset := torch.NewMNIST("./data")
+func ExampleMNIST() {
+	if e := downloadMNIST(); e != nil {
+		log.Printf("Cannot find or download MNIST dataset: %v", e)
+	}
+
+	dataset := torch.NewMNIST(dataDir())
 	dataset.AddTransforms([]torch.Transform{
 		torch.NewNormalize(0.1307, 0.3081),
 		torch.NewStack(),
@@ -19,4 +31,77 @@ func NotWorkingExampleMNIST() {
 	dataset.Close()
 	torch.FinishGC()
 	// Output:
+}
+
+const (
+	images = "train-images-idx3-ubyte"
+	labels = "train-labels-idx1-ubyte"
+	site   = "http://yann.lecun.com/exdb/mnist/"
+)
+
+func downloadMNIST() error {
+	if e := downloadIfNotYet(images); e != nil {
+		return e
+	}
+	if e := downloadIfNotYet(labels); e != nil {
+		return e
+	}
+	return nil
+}
+
+func downloadIfNotYet(fn string) error {
+	f := path.Join(dataDir(), fn)
+	if !fileExists(f) {
+		if e := download(site+fn+".gz", f); e != nil {
+			return e
+		}
+	}
+	return nil
+}
+
+func dataDir() string {
+	u, e := user.Current()
+	if e != nil {
+		return "testdata/mnist"
+	}
+	return path.Join(u.HomeDir, ".cache/mnist")
+}
+
+func fileExists(fn string) bool {
+	info, e := os.Stat(fn)
+	if os.IsNotExist(e) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func download(url, fn string) error {
+	resp, e := http.Get(url)
+	if e != nil {
+		return e
+	}
+	defer resp.Body.Close()
+
+	r, e := gzip.NewReader(resp.Body)
+	if e != nil {
+		return e
+	}
+	defer r.Close()
+
+	if e := os.MkdirAll(dataDir(), 0744); e != nil {
+		return e
+	}
+
+	f, e := os.Create(fn)
+	if e != nil {
+		return e
+	}
+	defer f.Close()
+
+	_, e = io.Copy(f, r)
+	if e != nil && e != io.EOF {
+		return e
+	}
+
+	return nil
 }
