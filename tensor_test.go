01abad0558561306289c49a057703ad2338cf186
qijun <qijun1994@hotmail.com>
2020-08-10 14:56:36 +0800

    clean code

--- tensor_test.go
+++ tensor_test.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	torch "github.com/wangkuiyi/gotorch"
-	"github.com/wangkuiyi/gotorch/nn/functional"
 )
 
 func ExampleTensor() {
@@ -13,21 +12,6 @@ func ExampleTensor() {
 	t.Close()
 	t.Close()
 	// Output:
-}
-
-func TestTranspose2d(t *testing.T) {
-	input := torch.RandN([]int{1, 1, 1}, false)
-	weight := torch.RandN([]int{1, 3, 3}, false)
-	var bias torch.Tensor
-	stride := []int{1}
-	padding := []int{0}
-	outputPadding := []int{0}
-	groups := 1
-	dilation := []int{1}
-	out := functional.ConvTranspose2d(input, weight, bias,
-		stride, padding, outputPadding, groups, dilation)
-	a := assert.New(t)
-	a.NotNil(out.T)
 }
 
 func TestLogSoftmax(t *testing.T) {
