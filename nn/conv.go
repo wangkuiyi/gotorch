0583ad2df254b72446ec8b4bd63c1722c210eeb4
QI JUN <qijun1994@hotmail.com>
2020-08-13 06:39:34 +0800

    add ConvTranspose2d module (#107)
    * init
    
    * add ConvTranspose2d module
    
    * clean comments
    
    * Make convTranspose2d compatible with new Module desing
    
    * Call Init from resetParameters
    
    Co-authored-by: Yi Wang <yi.wang.2005@gmail.com>

--- nn/conv.go
+++ nn/conv.go
@@ -57,3 +57,63 @@ func (c *Conv2d) Forward(x torch.Tensor) torch.Tensor {
 	return functional.Conv2d(x, c.Weight, c.Bias, []int64{c.Stride, c.Stride},
 		[]int64{c.Padding, c.Padding}, []int64{c.Dilation, c.Dilation}, c.Groups)
 }
+
+// ConvTranspose2dModule corresponds to torch.nn.ConvTranspose2d
+type ConvTranspose2dModule struct {
+	Module
+	InChannels  int64
+	OutChannels int64
+	KernelSize  int64
+	Stride      int64
+	Padding     int64
+	OutPadding  int64
+	Groups      int64
+	Dilation    int64
+	PaddingMode string
+	Weight      torch.Tensor
+	Bias        torch.Tensor
+}
+
+// ConvTranspose2d torch.nn.conv_transpose2d
+// TODO(qijun): only support zero padding mode
+// only support symmetry kernel/stride/padding/dilation
+// not support output_size when forwarding
+func ConvTranspose2d(inChannels, outChannels, kernelSize, stride, padding,
+	outPadding, groups int64, bias bool, dilation int64, paddingMode string) *ConvTranspose2dModule {
+	c := &ConvTranspose2dModule{
+		InChannels:  inChannels,
+		OutChannels: outChannels,
+		KernelSize:  kernelSize,
+		Stride:      stride,
+		Padding:     padding,
+		OutPadding:  outPadding,
+		Groups:      groups,
+		Dilation:    dilation,
+		PaddingMode: "zeros",
+	}
+	c.Weight = torch.Empty([]int64{inChannels, outChannels / groups, kernelSize,
+		kernelSize}, true)
+	if bias {
+		c.Bias = torch.Empty([]int64{outChannels}, true)
+	}
+	c.ResetParameters()
+	return c
+}
+
+// ResetParameters method
+func (c *ConvTranspose2dModule) ResetParameters() {
+	initializer.KaimingUniform(&c.Weight, math.Sqrt(5.0), "fan_in", "leaky_relu")
+	if c.Bias.T != nil {
+		fanIn, _ := initializer.CalculateFanInAndFanOut(c.Weight)
+		bound := 1.0 / math.Sqrt(float64(fanIn))
+		initializer.Uniform(&c.Bias, -bound, bound)
+	}
+	c.Init(c)
+}
+
+// Forward method
+func (c *ConvTranspose2dModule) Forward(x torch.Tensor) torch.Tensor {
+	return functional.ConvTranspose2d(x, c.Weight, c.Bias,
+		[]int64{c.Stride, c.Stride}, []int64{c.Padding, c.Padding},
+		[]int64{c.OutPadding, c.OutPadding}, c.Groups, []int64{c.Dilation, c.Dilation})
+}
