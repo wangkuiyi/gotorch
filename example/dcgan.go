0ef055a3abc1cd203ea494d9cb929111f5fe8e7c
qijun <qijun1994@hotmail.com>
2020-08-14 08:54:34 +0800

    clean code

--- example/dcgan.go
+++ example/dcgan.go
@@ -11,65 +11,35 @@ import (
 	"github.com/wangkuiyi/gotorch/vision"
 )
 
-// Generator network
-type Generator struct {
-	nn.Module
-	Main *nn.SequentialModule
+func newGenerator(nz int64) *nn.SequentialModule {
+	return nn.Sequential(
+		nn.ConvTranspose2d(nz, 256, 4, 1, 0, 0, 1, false, 1, "zero"),
+		nn.BatchNorm2d(256, 1e-5, 0.1, true, true),
+		nn.Functional(torch.Relu),
+		nn.ConvTranspose2d(256, 128, 3, 2, 1, 0, 1, false, 1, "zero"),
+		nn.BatchNorm2d(128, 1e-5, 0.1, true, true),
+		nn.Functional(torch.Relu),
+		nn.ConvTranspose2d(128, 64, 4, 2, 1, 0, 1, false, 1, "zero"),
+		nn.BatchNorm2d(64, 1e-5, 0.1, true, true),
+		nn.Functional(torch.Relu),
+		nn.ConvTranspose2d(64, 1, 4, 2, 1, 0, 1, false, 1, "zero"),
+		nn.Functional(torch.Tanh),
+	)
 }
 
-func newGenerator(nz int64) *Generator {
-	g := &Generator{
-		Main: nn.Sequential(
-			nn.ConvTranspose2d(nz, 256, 4, 1, 0, 0, 1, false, 1, "zero"),
-			nn.BatchNorm2d(256, 1e-5, 0.1, true, true),
-			nn.Functional(torch.Relu),
-			nn.ConvTranspose2d(256, 128, 3, 2, 1, 0, 1, false, 1, "zero"),
-			nn.BatchNorm2d(128, 1e-5, 0.1, true, true),
-			nn.Functional(torch.Relu),
-			nn.ConvTranspose2d(128, 64, 4, 2, 1, 0, 1, false, 1, "zero"),
-			nn.BatchNorm2d(64, 1e-5, 0.1, true, true),
-			nn.Functional(torch.Relu),
-			nn.ConvTranspose2d(64, 1, 4, 2, 1, 0, 1, false, 1, "zero"),
-			nn.Functional(torch.Tanh),
-		),
-	}
-	g.Init(g)
-	return g
-}
-
-// Forward method
-func (g *Generator) Forward(x torch.Tensor) torch.Tensor {
-	return g.Main.Forward(x).(torch.Tensor)
-}
-
-// Discriminator network
-type Discriminator struct {
-	nn.Module
-	Main *nn.SequentialModule
-}
-
-func newDiscriminator() *Discriminator {
-	d := &Discriminator{
-		Main: nn.Sequential(
-			nn.Conv2d(1, 64, 4, 2, 1, 1, 1, false, "zeros"),
-			nn.LeakyRelu(0.2, false),
-			nn.Conv2d(64, 128, 4, 2, 1, 1, 1, false, "zeros"),
-			nn.BatchNorm2d(128, 1e-5, 0.1, true, true),
-			nn.LeakyRelu(0.2, false),
-			nn.Conv2d(128, 256, 4, 2, 1, 1, 1, false, "zeros"),
-			nn.BatchNorm2d(256, 1e-5, 0.1, true, true),
-			nn.LeakyRelu(0.2, false),
-			nn.Conv2d(256, 1, 3, 1, 0, 1, 1, false, "zeros"),
-			nn.Functional(torch.Sigmoid),
-		),
-	}
-	d.Init(d)
-	return d
-}
-
-// Forward method
-func (d *Discriminator) Forward(x torch.Tensor) torch.Tensor {
-	return d.Main.Forward(x).(torch.Tensor).View([]int64{-1, 1}).Squeeze(1)
+func newDiscriminator() *nn.SequentialModule {
+	return nn.Sequential(
+		nn.Conv2d(1, 64, 4, 2, 1, 1, 1, false, "zeros"),
+		nn.LeakyRelu(0.2, false),
+		nn.Conv2d(64, 128, 4, 2, 1, 1, 1, false, "zeros"),
+		nn.BatchNorm2d(128, 1e-5, 0.1, true, true),
+		nn.LeakyRelu(0.2, false),
+		nn.Conv2d(128, 256, 4, 2, 1, 1, 1, false, "zeros"),
+		nn.BatchNorm2d(256, 1e-5, 0.1, true, true),
+		nn.LeakyRelu(0.2, false),
+		nn.Conv2d(256, 1, 3, 1, 0, 1, 1, false, "zeros"),
+		nn.Functional(torch.Sigmoid),
+	)
 }
 
 func main() {
@@ -105,16 +75,15 @@ func main() {
 			optimizerD.ZeroGrad()
 			label := torch.Empty([]int64{batch.Data.Shape()[0]}, false)
 			initializer.Uniform(&label, 0.8, 1.0)
-
-			output := netD.Forward(batch.Data)
+			output := netD.Forward(batch.Data).(torch.Tensor).View([]int64{-1, 1}).Squeeze(1)
 			errDReal := F.BinaryCrossEntropy(output, label, torch.Tensor{}, "mean")
 			errDReal.Backward()
 
 			// train with fake
 			noise := torch.RandN([]int64{batch.Data.Shape()[0], nz, 1, 1}, false)
-			fake := netG.Forward(noise)
+			fake := netG.Forward(noise).(torch.Tensor)
 			initializer.Zeros(&label)
-			output = netD.Forward(fake.Detach())
+			output = netD.Forward(fake.Detach()).(torch.Tensor).View([]int64{-1, 1}).Squeeze(1)
 			errDFake := F.BinaryCrossEntropy(output, label, torch.Tensor{}, "mean")
 			errDFake.Backward()
 			errD := errDReal.Item() + errDFake.Item()
@@ -123,14 +92,15 @@ func main() {
 			// (2) update G network
 			optimizerG.ZeroGrad()
 			initializer.Ones(&label)
-			output = netD.Forward(fake)
+			output = netD.Forward(fake).(torch.Tensor).View([]int64{-1, 1}).Squeeze(1)
 			errG := F.BinaryCrossEntropy(output, label, torch.Tensor{}, "mean")
 			errG.Backward()
 			optimizerG.Step()
-			fmt.Printf("[%d/%d][%d] Loss_D: %f Loss_G: %f\n",
+
+			fmt.Printf("[%d/%d][%d] D_Loss: %f G_Loss: %f\n",
 				epoch, epochs, i, errD, errG.Item())
 			if i%checkpointStep == 0 {
-				samples := netG.Forward(torch.RandN([]int64{10, nz, 1, 1}, false))
+				samples := netG.Forward(torch.RandN([]int64{10, nz, 1, 1}, false)).(torch.Tensor)
 				ckName := fmt.Sprintf("dcgan-sample-%d.pt", checkpointCount)
 				samples.Detach().Save(ckName)
 				checkpointCount++
@@ -141,5 +111,4 @@ func main() {
 	}
 	mnist.Close()
 	torch.FinishGC()
-	// Output:
 }
