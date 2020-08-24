927edfa726f1181a8836ca8c8a4084a24e4c313f
Yan Xu <yancey1989@gmail.com>
2020-08-24 22:18:51 +0800

    Add ToTensor function (#187)
    * add to_tensor function
    
    * update
    
    * update

--- example/resnet/imagenet/loader_test.go
+++ example/resnet/imagenet/loader_test.go
@@ -2,6 +2,7 @@ package imagenet_test
 
 import (
 	"bytes"
+	"image/color"
 	"io"
 	"testing"
 
@@ -16,10 +17,29 @@ func TestDataloader(t *testing.T) {
 	vocab, err := imagenet.BuildLabelVocabulary(&tgz1)
 	assert.NoError(t, err)
 
-	loader, err := imagenet.NewDataLoader(&tgz2, vocab, 4)
+	loader, err := imagenet.NewDataLoader(&tgz2, vocab, 2)
 	assert.NoError(t, err)
 
 	for loader.Scan() {
-		loader.Minibatch()
+		data, label := loader.Minibatch()
+		assert.Equal(t, []int64{2, 469, 387, 3}, data.Shape())
+		assert.Equal(t, []int64{2, 1}, label.Shape())
+	}
+}
+
+func TestToTensor(t *testing.T) {
+	{
+		// image to Tensor
+		blue := color.RGBA{0, 0, 255, 255}
+		m := imagenet.SynthesizeImage(4, 4, blue)
+		out, err := imagenet.ToTensor(m)
+		assert.NoError(t, err)
+		assert.Equal(t, out.Shape(), []int64{4, 4, 3})
+	}
+	{
+		// int to Tensor
+		out, err := imagenet.ToTensor(10)
+		assert.NoError(t, err)
+		assert.Equal(t, out.Shape(), []int64{1})
 	}
 }
