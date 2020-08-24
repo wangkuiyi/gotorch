40a111371327703419d4b9b78054943d534a3954
Yancey1989 <yancey1989@gmail.com>
2020-08-24 21:43:25 +0800

    update

--- example/resnet/imagenet/loader.go
+++ example/resnet/imagenet/loader.go
@@ -150,13 +150,17 @@ func ToTensor(obj interface{}) (torch.Tensor, error) {
 	}
 }
 
+// ToTensor transform c.f. https://github.com/pytorch/vision/blob/ba1b22125723f3719a3c38a2fe7cd6fb77657c57/torchvision/transforms/functional.py#L45
 func imageToTensor(img image.Image) torch.Tensor {
 	width, height := img.Bounds().Max.X, img.Bounds().Max.Y
+	// put pixel values with HWC format
 	array := make([][][3]float32, height)
 
 	for x := 0; x < height; x++ {
 		row := make([][3]float32, width)
 		for y := 0; y < width; y++ {
+			// ResNet need the 3 channels image, here we should convert to RGB format.
+			// The division by 255.0 is applied to convert RGB pixel values from [0, 255] to [0.0, 1.0] range
 			c := img.(*image.RGBA).RGBAAt(x, y)
 			row[y] = [3]float32{float32(c.R / 255.0), float32(c.G / 255.0), float32(c.B / 255.0)}
 		}
