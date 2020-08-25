b94101c601d2883ad2be12d636c3e8e7a851ed15
typhoonzero <typhoonzero1986@gmail.com>
2020-08-25 11:42:33 +0800

    polish coverage

--- vision/transforms/random_crop_test.go
+++ vision/transforms/random_crop_test.go
@@ -28,7 +28,6 @@ func TestRandomCrop(t *testing.T) {
 	a := assert.New(t)
 
 	m := generateRandImage(image.Rect(0, 0, 200, 200))
-
 	trans := RandomCrop(100, 100)
 	cropped, err := trans.Run(m)
 	a.NoError(err)
@@ -38,4 +37,16 @@ func TestRandomCrop(t *testing.T) {
 	}
 	a.Equal(100, i.Bounds().Max.X)
 	a.Equal(100, i.Bounds().Max.Y)
+}
+
+func TestRandomCropSizeError(t *testing.T) {
+	a := assert.New(t)
+
+	m := generateRandImage(image.Rect(0, 0, 200, 200))
+	trans := RandomCrop(300, 300)
+
+	_, err := trans.Run("some string")
+	a.Error(err)
+	_, err = trans.Run(m)
+	a.Error(err)
 }
