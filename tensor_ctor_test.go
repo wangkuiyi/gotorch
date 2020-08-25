6960dfe126aabc1e46f0368e643f456b6e15cf2f
Yi Wang <yi.wang.2005@gmail.com>
2020-08-25 10:41:51 -0700

    Simplify code but keep the error

--- tensor_ctor_test.go
+++ tensor_ctor_test.go
@@ -10,26 +10,17 @@ import (
 func TestRandN(t *testing.T) {
 	a := torch.RandN([]int64{10, 100}, false)
 	assert.Equal(t, []int64{10, 100}, a.Shape())
-	assert.NotPanics(t, func() {
-		a.Close()
-		a.Close()
-	})
+	a.Close()
 }
 
 func TestRand(t *testing.T) {
 	a := torch.Rand([]int64{50, 100}, false)
 	assert.Equal(t, []int64{50, 100}, a.Shape())
-	assert.NotPanics(t, func() {
-		a.Close()
-		a.Close()
-	})
+	a.Close()
 }
 
 func TestEmpty(t *testing.T) {
 	a := torch.Empty([]int64{50, 10}, false)
 	assert.Equal(t, []int64{50, 10}, a.Shape())
-	assert.NotPanics(t, func() {
-		a.Close()
-		a.Close()
-	})
+	a.Close()
 }
