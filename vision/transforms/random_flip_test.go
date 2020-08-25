53019846c223ce02ce913d5141bb263142c0d408
typhoonzero <typhoonzero1986@gmail.com>
2020-08-25 14:36:46 +0800

    update ut

--- vision/transforms/random_flip_test.go
+++ vision/transforms/random_flip_test.go
@@ -9,13 +9,19 @@ import (
 
 func TestRandomFlip(t *testing.T) {
 	a := assert.New(t)
-	i := generateRandImage(image.Rect(0, 0, 200, 200))
+	i := generateRandImage(image.Rect(0, 0, 50, 50))
 	width := i.Bounds().Max.X
 
 	trans := RandomFlip()
-	o, err := trans.Run(i)
-	a.NoError(err)
-	outImage := o.(*image.NRGBA)
-	inImage := i.(*image.NRGBA)
-	a.True(inImage.At(0, 0) == outImage.At(0, 0) || inImage.At(0, 0) == outImage.At(width-1, 0))
+	_, err := trans.Run(100)
+	a.Error(err)
+
+	// Run 10 times to cover random cases
+	for j := 0; j < 10; j++ {
+		o, err := trans.Run(i)
+		a.NoError(err)
+		outImage := o.(*image.NRGBA)
+		inImage := i.(*image.NRGBA)
+		a.True(inImage.At(0, 0) == outImage.At(0, 0) || inImage.At(0, 0) == outImage.At(width-1, 0))
+	}
 }
