77005d8d1a4f07e4297b387171db4de6f3731055
Yi Wang <yi.wang.2005@gmail.com>
2020-08-25 13:48:35 -0700

    Add test_ctr_test.go (#209)
    * Add test_ctr_test.go
    
    * Simplify code but keep the error
    
    * Remove call to Tensor.Close

---
+++ tensor_ctor_test.go
@@ -0,0 +1,23 @@
+package gotorch_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	torch "github.com/wangkuiyi/gotorch"
+)
+
+func TestRandN(t *testing.T) {
+	a := torch.RandN([]int64{10, 100}, false)
+	assert.Equal(t, []int64{10, 100}, a.Shape())
+}
+
+func TestRand(t *testing.T) {
+	a := torch.Rand([]int64{50, 100}, false)
+	assert.Equal(t, []int64{50, 100}, a.Shape())
+}
+
+func TestEmpty(t *testing.T) {
+	a := torch.Empty([]int64{50, 10}, false)
+	assert.Equal(t, []int64{50, 10}, a.Shape())
+}
