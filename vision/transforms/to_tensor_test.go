7a3977280605b4c2ea4a2a3c2365987f31cd6065
Yi Wang <yi.wang.2005@gmail.com>
2020-08-26 13:11:04 -0700

    Fix bug

--- vision/transforms/to_tensor_test.go
+++ vision/transforms/to_tensor_test.go
@@ -15,11 +15,15 @@ func TestToTensor(t *testing.T) {
 	trans := ToTensor()
 	out := trans.Run(m)
 	a.Equal(out.Shape(), []int64{3, 2, 2})
-	t.Log(out.String())
+	a.Equal("(1,.,.) = \n  0  0\n  0  0\n\n(2,.,.) = \n  0  0\n  0  0\n\n(3,.,.) = \n  1  1\n  1  1\n[ CPUFloatType{3,2,2} ]",
+		out.String())
 
 	// int to Tensor
 	out = trans.Run(10)
 	a.Equal(out.Shape(), []int64{1})
+	a.Equal(" 10\n[ CPUIntType{1} ]", out.String())
+
+	// converting any other type to tensor would panic.
 	a.Panics(func() {
 		trans.Run("hello")
 	})
