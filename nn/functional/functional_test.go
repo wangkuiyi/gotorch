3d41a30def5d283b484d81357d2d180f1a7d1d71
Yi Wang <yi.wang.2005@gmail.com>
2020-08-25 20:14:42 -0700

    Add cases to nn/functional/functional_test.go (#212)
    * Add some tests
    
    * Update

--- nn/functional/functional_test.go
+++ nn/functional/functional_test.go
@@ -7,7 +7,34 @@ import (
 	torch "github.com/wangkuiyi/gotorch"
 )
 
-func TestTranspose2d(t *testing.T) {
+func TestBatchNorm(t *testing.T) {
+	input := torch.RandN([]int64{10, 20}, true)
+	w := torch.RandN([]int64{20}, true)
+	r := BatchNorm(input, torch.Tensor{}, torch.Tensor{}, w, torch.Tensor{}, true, 0.1, 0.1)
+	assert.NotNil(t, r.T)
+}
+
+/*
+filters = torch.randn(8,4,3,3)
+inputs = torch.randn(1,4,5,5)
+o = F.conv2d(inputs, filters, padding=1)
+*/
+func TestConv2d(t *testing.T) {
+	filters := torch.RandN([]int64{8, 4, 3, 3}, false)
+	inputs := torch.RandN([]int64{1, 4, 5, 5}, false)
+	var bias torch.Tensor
+	o := Conv2d(inputs, filters, bias, []int64{1, 1}, []int64{1, 1}, []int64{1, 1}, 1)
+	assert.NotNil(t, o.T)
+	assert.Equal(t, []int64{1, 8, 5, 5}, o.Shape())
+}
+
+/*
+import torch.nn.functional as F
+inputs = torch.randn(1, 4, 5, 5)
+weights = torch.randn(4, 8, 3, 3)
+o = F.conv_transpose2d(inputs, weights, padding=1)
+*/
+func TestConvTranspose2d(t *testing.T) {
 	input := torch.RandN([]int64{1, 4, 5, 5}, false)
 	weight := torch.RandN([]int64{4, 8, 3, 3}, false)
 	var bias torch.Tensor
@@ -19,13 +46,18 @@ func TestTranspose2d(t *testing.T) {
 	out := ConvTranspose2d(input, weight, bias,
 		stride, padding, outputPadding, groups, dilation)
 	assert.NotNil(t, out.T)
+	assert.Equal(t, []int64{1, 8, 5, 5}, out.Shape())
 }
 
-func TestBatchNorm(t *testing.T) {
-	input := torch.RandN([]int64{10, 20}, true)
-	w := torch.RandN([]int64{20}, true)
-	r := BatchNorm(input, torch.Tensor{}, torch.Tensor{}, w, torch.Tensor{}, true, 0.1, 0.1)
-	assert.NotNil(t, r.T)
+/*
+# input is of size N x C = 3 x 5
+input = torch.randn(3, 5, requires_grad=True)
+# each element in target has to have 0 <= value < C
+target = torch.tensor([1, 0, 4])
+output = F.nll_loss(F.log_softmax(input), target)
+output.backward()
+*/
+func TestNllLoss(t *testing.T) {
 }
 
 func TestBinaryCrossEntropy(t *testing.T) {
