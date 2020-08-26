6346e9f3a92fdf2b128b3e756a672944ceab819a
Yi Wang <yi.wang.2005@gmail.com>
2020-08-26 15:18:53 -0700

    Rewrite RandomFlip following PyTorch APIs (#224)
    * Update
    
    * Resolve conflicts
    
    * Rewrite random_flip
    
    * Rename testing_test.go
    
    * Update uses

--- vision/transforms/testing_test.go
+++ vision/transforms/testing_test.go
@@ -33,3 +33,22 @@ func colorEqual(x, y color.Color) bool {
 	r2, b2, g2, a2 := y.RGBA()
 	return r1 == r2 && b1 == b2 && g1 == g2 && a1 == a2
 }
+
+var (
+	red   = color.RGBA{255, 0, 0, 255}
+	green = color.RGBA{0, 255, 0, 255}
+	blue  = color.RGBA{0, 0, 255, 255}
+	white = color.RGBA{255, 255, 255, 255}
+)
+
+// Draw a 2x2 pixel image:
+//  red | green
+//  blue | white
+func drawRGBWImage() image.Image {
+	m := draw.Image(image.NewRGBA(image.Rect(0, 0, 2, 2)))
+	m.Set(0, 0, red)
+	m.Set(0, 1, green)
+	m.Set(1, 0, blue)
+	m.Set(1, 1, white)
+	return m
+}
