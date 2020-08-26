19f6a3bae5a9dbe2e7d0a9f591c9df76ff0a902d
Yi Wang <yi.wang.2005@gmail.com>
2020-08-26 14:50:53 -0700

    Update

--- vision/transforms/center_crop_test.go
+++ vision/transforms/center_crop_test.go
@@ -2,6 +2,7 @@ package transforms
 
 import (
 	"image"
+	"image/color"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,18 +11,25 @@ import (
 func TestCenterCrop(t *testing.T) {
 	a := assert.New(t)
 
-	i := generateRandImage(image.Rect(0, 0, 200, 200))
-	a.Panics(func() {
-		trans := CenterCrop(50, 250)
-		trans.Run(i)
-	})
+	blue := color.RGBA{0, 0, 255, 255}
+	m := drawImage(image.Rect(0, 0, 2, 2), blue)
 
-	trans := CenterCrop(50, 50)
-	o := trans.Run(i)
-	outImage := o.(image.Image)
-	a.Equal(50, outImage.Bounds().Max.X)
-	startX := (200 - 50) / 2
-	startY := (200 - 50) / 2
-	a.Equal(i.At(startX, startY), outImage.At(0, 0))
+	r := CenterCrop(2, 2).Run(m)
+	a.Equal(image.Rectangle{Min: image.Point{0, 0}, Max: image.Point{2, 2}}, r.Bounds())
+	a.Equal(blue, r.At(0, 0))
+
+	r = CenterCrop(1, 2).Run(m)
+	a.Equal(image.Rectangle{Min: image.Point{0, 0}, Max: image.Point{2, 1}}, r.Bounds())
+
+	a.Panics(func() { CenterCrop(3, 1).Run(m) })
+	a.Panics(func() { CenterCrop(1, 3).Run(m) })
+
+	// trans := CenterCrop(50, 50)
+	// o := trans.Run(m)
+	// outImage := o.(image.Image)
+	// a.Equal(50, outImage.Bounds().Max.X)
+	// startX := (200 - 50) / 2
+	// startY := (200 - 50) / 2
+	//	a.Equal(i.At(startX, startY), outImage.At(0, 0))
 
 }
