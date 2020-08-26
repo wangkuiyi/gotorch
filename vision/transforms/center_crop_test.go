d8b7065ada4f0351389367e39269658cb136a2e2
Yi Wang <yi.wang.2005@gmail.com>
2020-08-26 14:52:52 -0700

    Resolve conflicts

--- vision/transforms/center_crop_test.go
+++ vision/transforms/center_crop_test.go
@@ -26,15 +26,10 @@ func TestCenterCrop(t *testing.T) {
 	a.True(colorEqual(blue, r.At(0, 0)))
 	a.True(colorEqual(blue, r.At(1, 0)))
 
+	r = CenterCrop(1, 1).Run(m)
+	a.Equal(image.Rectangle{Min: image.Point{0, 0}, Max: image.Point{1, 1}}, r.Bounds())
+	a.True(colorEqual(blue, r.At(0, 0)))
+
 	a.Panics(func() { CenterCrop(3, 1).Run(m) })
 	a.Panics(func() { CenterCrop(1, 3).Run(m) })
-
-	// trans := CenterCrop(50, 50)
-	// o := trans.Run(m)
-	// outImage := o.(image.Image)
-	// a.Equal(50, outImage.Bounds().Max.X)
-	// startX := (200 - 50) / 2
-	// startY := (200 - 50) / 2
-	//	a.Equal(i.At(startX, startY), outImage.At(0, 0))
-
 }
