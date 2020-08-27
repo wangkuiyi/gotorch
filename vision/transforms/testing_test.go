985d9339a939917abf031605f8cd943745ee5b0f
Yi Wang <yi.wang.2005@gmail.com>
2020-08-26 19:30:35 -0700

    Remove no longer in use function generateRandImage (#226)
    * Remove no longer in use function generateRandImage
    
    * Remove unused imports

--- vision/transforms/testing_test.go
+++ vision/transforms/testing_test.go
@@ -4,23 +4,7 @@ import (
 	"image"
 	"image/color"
 	"image/draw"
-	"math/rand"
-	"time"
 )
-
-func generateRandImage(size image.Rectangle) image.Image {
-	r0 := rand.New(rand.NewSource(int64(time.Now().Second())))
-	i := image.NewNRGBA(size)
-	for y := 0; y < size.Max.Y-1; y++ {
-		for x := 0; x < size.Max.X-1; x++ {
-			a0 := uint8(r0.Float32() * 255)
-			rgb0 := uint8(r0.Float32() * 255)
-			rgb0 = rgb0 * a0
-			i.Set(x, y, color.RGBA{rgb0, rgb0, rgb0, a0})
-		}
-	}
-	return i
-}
 
 func drawImage(size image.Rectangle, c color.Color) image.Image {
 	m := image.NewRGBA(size)
