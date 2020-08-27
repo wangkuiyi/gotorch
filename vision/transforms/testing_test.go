eefd4b652e9cb5168090df271afc85dcd76e731e
Yi Wang <yi.wang.2005@gmail.com>
2020-08-26 17:48:58 -0700

    Remove no longer in use function generateRandImage

--- vision/transforms/testing_test.go
+++ vision/transforms/testing_test.go
@@ -8,20 +8,6 @@ import (
 	"time"
 )
 
-func generateRandImage(size image.Rectangle) image.Image {
-	r0 := rand.New(rand.NewSource(int64(time.Now().Second())))
-	i := image.NewNRGBA(size)
-	for y := 0; y < size.Max.Y-1; y++ {
-		for x := 0; x < size.Max.X-1; x++ {
-			a0 := uint8(r0.Float32() * 255)
-			rgb0 := uint8(r0.Float32() * 255)
-			rgb0 = rgb0 * a0
-			i.Set(x, y, color.RGBA{rgb0, rgb0, rgb0, a0})
-		}
-	}
-	return i
-}
-
 func drawImage(size image.Rectangle, c color.Color) image.Image {
 	m := image.NewRGBA(size)
 	draw.Draw(m, m.Bounds(), &image.Uniform{c}, image.ZP, draw.Src)
