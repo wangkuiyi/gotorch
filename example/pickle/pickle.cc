15d0e3f04265200ead6f92e177f9ef34e0db1d59
Yi Wang <yi.wang.2005@gmail.com>
2020-08-28 11:22:17 -0700

    Make example/pickle prints MD5 of the encoding

--- example/pickle/pickle.cc
+++ example/pickle/pickle.cc
@@ -1,4 +1,6 @@
 // Copyright 2020. GoTorch Authors.
+#include <openssl/md5.h>
+
 #include <iostream>
 
 #include "torch/csrc/api/include/torch/serialize.h"
@@ -14,6 +16,13 @@ int main() {
 
   auto e = torch::pickle_save(a);
   std::cout << "Encoded buffer size = " << e.size() << std::endl;
+
+  unsigned char checksum[MD5_DIGEST_LENGTH];
+  MD5(reinterpret_cast<unsigned char*>(e.data()), e.size(), checksum);
+  for (unsigned char c : checksum) {
+    std::cout << std::hex << static_cast<unsigned int>(c);
+  }
+  std::cout << std::endl;
 
   auto v = torch::pickle_load(e);
   std::cout << "Loaded tensor = " << v << std::endl;
