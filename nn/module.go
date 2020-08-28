a1b95ab0d0ecb26a40a821941b13d032905cbb05
Yi Wang <yi.wang.2005@gmail.com>
2020-08-28 15:12:42 -0700

    Refactorize Module to have visitTenosrs and Visitor type

--- nn/module.go
+++ nn/module.go
@@ -197,7 +197,8 @@ func (m *Module) ZeroGrad() {
 func (m *Module) NamedParameters() map[string]torch.Tensor {
 	must(m.outer != nil, "GoTorch modules requires calling `Init` before using")
 	r := make(map[string]torch.Tensor)
-	getNamedNonNilTensors(m.outer, reflect.TypeOf(m.outer).Elem().Name(), true, false, r)
+	visitTensors(m.outer, reflect.TypeOf(m.outer).Elem().Name(),
+		makeTensorRecorder(r, true, false))
 	return r
 }
 
@@ -205,62 +206,9 @@ func (m *Module) NamedParameters() map[string]torch.Tensor {
 func (m *Module) NamedBuffers() map[string]torch.Tensor {
 	must(m.outer != nil, "GoTorch modules requires calling `Init` before using")
 	r := make(map[string]torch.Tensor)
-	getNamedNonNilTensors(m.outer, reflect.TypeOf(m.outer).Elem().Name(), false, true, r)
+	visitTensors(m.outer, reflect.TypeOf(m.outer).Elem().Name(),
+		makeTensorRecorder(r, false, true))
 	return r
-}
-
-func getNamedNonNilTensors(m IModule, prefix string, param, buffer bool, r map[string]torch.Tensor) {
-	if reflect.ValueOf(m).IsNil() {
-		return
-	}
-	moduleType := reflect.TypeOf((*IModule)(nil)).Elem()
-	sv := reflect.ValueOf(m).Elem() // Elem gets what the pointer points to.
-	for i := 0; i < sv.NumField(); i++ {
-		f := sv.Type().Field(i)
-		v := sv.Field(i)
-		if v.Kind() == reflect.Slice || v.Kind() == reflect.Array {
-			must(v.CanInterface(),
-				"GoTorch requires exporting Module field %s.%s", sv.Type().Name(), f.Name)
-			for j := 0; j < v.Len(); j++ {
-				getNamedNonNilTensors(v.Index(j).Interface().(IModule),
-					fmt.Sprintf("%s.%s[%d]", prefix, f.Name, j), param, buffer, r)
-			}
-		} else if f.Type.Implements(moduleType) {
-			must(v.CanInterface(),
-				"GoTorch requires exporting Module field %s.%s", sv.Type().Name(), f.Name)
-			getNamedNonNilTensors(v.Interface().(IModule),
-				prefix+"."+f.Name, param, buffer, r)
-		} else {
-			recordNonNilTensor(f, v, prefix, r, param, buffer)
-		}
-	}
-}
-
-// If field f is a parameter or buffer field and the value v is not a nil
-// tensor, insert v into map r with key is prefix+"."+f.Name.
-func recordNonNilTensor(f reflect.StructField, v reflect.Value,
-	prefix string, r map[string]torch.Tensor, param, buffer bool) {
-	tensorType := reflect.TypeOf((*torch.Tensor)(nil)).Elem()
-	if f.Type != tensorType {
-		return // Either parameter or buffer is of type Tensor.
-	}
-
-	tag := f.Tag.Get("gotorch")
-	if !buffer && tag == "buffer" {
-		return // Don't wants a buffer but this field is one.
-	}
-	if !param && (tag == "param" || tag == "") {
-		return // Don't wants a parameter but this field is one.
-	}
-
-	must(v.CanInterface(),
-		"GoTorch requires exporting Tensor field %s.%s", v.Type().Name(), f.Name)
-	fv := v.Interface().(torch.Tensor)
-	if fv.T == nil {
-		return // Don't record nil Tensor
-	}
-
-	r[prefix+"."+f.Name] = fv
 }
 
 // Parameters returns trainable parameters (recursively)
@@ -283,10 +231,67 @@ func (m *Module) Buffers() []torch.Tensor {
 	return result
 }
 
-func must(condition bool, fmtStr string, args ...interface{}) {
-	if !condition {
-		// Use logPanicf to be recoverable and enable stack trace
-		log.Panicf(fmtStr, args...)
+// Visitor is a function type supposed to be called by visitTensors.  The
+// parameter f and v are a tensor-typed field in a given module.  In most cases,
+// it should return true; otherwise, it breaks the recursive visiting process.
+type Visitor func(f reflect.StructField, v reflect.Value, prefix string) bool
+
+func visitTensors(m IModule, prefix string, visitor Visitor) {
+	if reflect.ValueOf(m).IsNil() {
+		return // No need to visit fields in a nil Module value.
+	}
+
+	moduleType := reflect.TypeOf((*IModule)(nil)).Elem()
+	tensorType := reflect.TypeOf((*torch.Tensor)(nil)).Elem()
+
+	sv := reflect.ValueOf(m).Elem() // Elem gets what the pointer points to.
+	for i := 0; i < sv.NumField(); i++ {
+		f := sv.Type().Field(i)
+		v := sv.Field(i)
+
+		switch {
+		case (v.Kind() == reflect.Slice || v.Kind() == reflect.Array):
+			// The field is a slice or array.
+			must(v.CanInterface(), "Please export slice and array field %s.%s",
+				sv.Type().Name(), prefix+f.Name)
+			for j := 0; j < v.Len(); j++ {
+				visitTensors(v.Index(j).Interface().(IModule),
+					fmt.Sprintf("%s.%s[%d]", prefix, f.Name, j), visitor)
+			}
+
+		case f.Type.Implements(moduleType):
+			// The field is of type Module.
+			must(v.CanInterface(), "Please export Module field %s.%s",
+				sv.Type().Name(), prefix+f.Name)
+			visitTensors(v.Interface().(IModule), prefix+"."+f.Name, visitor)
+
+		case f.Type == tensorType:
+			// The field is of type Tensor.
+			if r := visitor(f, v, prefix); !r {
+				return
+			}
+		}
+	}
+}
+
+// makeTensorRecorder returns a visitor function that records a parameter and/or
+// buffer tensor in a module into map record with key set to prefix+"."+f.Name.
+func makeTensorRecorder(record map[string]torch.Tensor, param, buffer bool) Visitor {
+	return func(f reflect.StructField, v reflect.Value, prefix string) bool {
+		must(v.CanInterface(), "Please export Tensor field %s.%s",
+			v.Type().Name(), prefix+f.Name)
+
+		tag := f.Tag.Get("gotorch")
+		if (buffer && tag == "buffer") || (param && (tag == "param" || tag == "")) {
+			// If the field is what we want.
+			fv := v.Interface().(torch.Tensor)
+			if fv.T != nil {
+				// Don't record nil Tensor, for example,
+				// unspecified bias of module Linear.
+				record[prefix+"."+f.Name] = fv
+			}
+		}
+		return true // Alway go on visiting.
 	}
 }
 
@@ -295,6 +300,27 @@ func must(condition bool, fmtStr string, args ...interface{}) {
 func (m *Module) StateDict() map[string]torch.Tensor {
 	must(m.outer != nil, "GoTorch modules requires calling `Init` before using")
 	r := make(map[string]torch.Tensor)
-	getNamedNonNilTensors(m.outer, reflect.TypeOf(m.outer).Elem().Name(), true, true, r)
+	visitTensors(m.outer, reflect.TypeOf(m.outer).Elem().Name(),
+		makeTensorRecorder(r, true, true))
 	return r
+}
+
+// func (m *Module) SetStateDict(sd map[string]torch.Tensor) error {
+// 	must(m.outer != nil, "GoTorch modules requires calling `Init` before using")
+
+// 	// SetStateDict requires that (1) entries in the map are all defined in
+// 	// the module, and (2) parameters and buffers in the module are all in
+// 	// the map.  To ensure (2), we go over module fields using Go
+// 	// reflection, and for each field, we check the existence of the
+// 	// corresponding entry in the map.  To ensure (2), we store fields in a
+// 	// set, and check that every entry in the map is in the set.
+// 	marks := make(map[string]int)
+
+// }
+
+func must(condition bool, fmtStr string, args ...interface{}) {
+	if !condition {
+		// Use logPanicf to be recoverable and enable stack trace
+		log.Panicf(fmtStr, args...)
+	}
 }
