096f6326d8245a0e00a1b4378b27c0864576228e
Yan Xu <yancey1989@gmail.com>
2020-08-29 09:38:54 +0800

    Resnet training skips samples at the begging of each epoch (#239)
    * train with imagenet loader inifinit
    
    * skip samples on imagenet loader
    
    * fix ut
    
    * update
    
    * update
    
    * polish code
    
    * update
    
    * update
    
    * add opt.Zerograd

--- vision/datasets/imagenet_test.go
+++ vision/datasets/imagenet_test.go
@@ -20,7 +20,7 @@ func TestImgNetLoader(t *testing.T) {
 	a.Equal(3, len(vocab))
 
 	trans := transforms.Compose(transforms.RandomCrop(224, 224), transforms.RandomHorizontalFlip(0.5), transforms.ToTensor())
-	loader, e := datasets.ImageNet(bytes.NewReader(tgz.Bytes()), vocab, trans, 2)
+	loader, e := datasets.ImageNet(bytes.NewReader(tgz.Bytes()), vocab, trans, 2, 0)
 	a.NoError(e)
 	{
 		// the first iteration
@@ -46,4 +46,27 @@ func TestImgNetLoader(t *testing.T) {
 func TestBuildLabelVocabularyFail(t *testing.T) {
 	_, e := datasets.BuildLabelVocabulary(strings.NewReader("some string"))
 	assert.Error(t, e)
+}
+func TestImgNetLoaderSkipSamples(t *testing.T) {
+	a := assert.New(t)
+	var tgz bytes.Buffer
+	synthesizeImages(&tgz)
+
+	vocab, e := datasets.BuildLabelVocabulary(bytes.NewReader(tgz.Bytes()))
+	a.NoError(e)
+	a.Equal(3, len(vocab))
+
+	trans := transforms.Compose(transforms.RandomCrop(224, 224), transforms.RandomHorizontalFlip(0.5), transforms.ToTensor())
+	loader, e := datasets.ImageNet(bytes.NewReader(tgz.Bytes()), vocab, trans, 2, 2)
+	a.NoError(e)
+	{
+		// the first iteration
+		a.True(loader.Scan())
+		data, label := loader.Minibatch()
+		a.Equal([]int64{1, 3, 224, 224}, data.Shape())
+		a.Equal([]int64{1}, label.Shape())
+		a.NoError(loader.Err())
+	}
+	// no more data
+	a.False(loader.Scan())
 }
