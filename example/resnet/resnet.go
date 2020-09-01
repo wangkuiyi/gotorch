02e97104aefc8c8c63f1de507e22482fc42aca1a
Yancey1989 <yancey1989@gmail.com>
2020-09-01 15:56:21 +0800

    imageloader to loader imagenet dataset

--- example/resnet/resnet.go
+++ example/resnet/resnet.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"encoding/gob"
 	"flag"
-	"io"
+	"fmt"
+	"image"
 	"log"
 	"math"
-	"math/rand"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	torch "github.com/wangkuiyi/gotorch"
 	F "github.com/wangkuiyi/gotorch/nn/functional"
+	"github.com/wangkuiyi/gotorch/nn/initializer"
 	"github.com/wangkuiyi/gotorch/vision/datasets"
 	"github.com/wangkuiyi/gotorch/vision/models"
 	"github.com/wangkuiyi/gotorch/vision/transforms"
@@ -73,18 +77,18 @@ func accuracy(output, target torch.Tensor, topk []int64) []float32 {
 	return res
 }
 
-func imageNetLoader(r io.Reader, vocab map[string]int64, mbSize int, skipSamples int) (*datasets.ImageNetLoader, error) {
+func imageNetLoader(fn string, vocab map[string]int64, mbSize int) *datasets.ImageLoader {
 	trans := transforms.Compose(
 		transforms.RandomResizedCrop(224),
 		transforms.RandomHorizontalFlip(0.5),
 		transforms.ToTensor(),
 		transforms.Normalize([]float64{0.485, 0.456, 0.406}, []float64{0.229, 0.224, 0.225}))
 
-	loader, e := datasets.ImageNet(r, vocab, trans, mbSize, skipSamples)
+	loader, e := datasets.NewImageLoader(fn, vocab, trans, mbSize)
 	if e != nil {
-		return nil, e
+		log.Fatal(e)
 	}
-	return loader, nil
+	return loader
 }
 
 func trainOneMinibatch(image, target torch.Tensor, model *models.ResnetModule, opt torch.Optimizer) (float32, float32, float32) {
@@ -98,18 +102,136 @@ func trainOneMinibatch(image, target torch.Tensor, model *models.ResnetModule, o
 	return loss.Item().(float32), acc1, acc5
 }
 
-func main() {
-	trainFile := flag.String("train-file", "train.tgz", "training images folder with tgz compressed.")
-	flag.Parse()
-	if _, e := os.Open(*trainFile); e != nil {
-		panic(e)
+func test(model *models.ResnetModule, loader *datasets.ImageLoader) {
+	testLoss := float32(0)
+	correct := int64(0)
+	samples := 0
+	for loader.Scan() {
+		data, label := loader.Minibatch()
+		data = data.To(device, data.Dtype())
+		label = label.To(device, label.Dtype())
+		output := model.Forward(data)
+		loss := F.NllLoss(output, label, torch.Tensor{}, -100, "mean")
+		pred := output.Argmax(1)
+		testLoss += loss.Item().(float32)
+		correct += pred.Eq(label.View(pred.Shape())).SumByDim(0, false).Item().(int64)
+		samples += int(label.Shape()[0])
 	}
+	log.Printf("Test average loss: %.4f, Accuracy: %.2f%%\n",
+		testLoss/float32(samples), 100.0*float32(correct)/float32(samples))
+}
 
-	mbSize := 32 // minibatch size
-	epochs := 100
+func train(trainFn, testFn, save string, epochs int) {
+	// build label vocabulary
+	vocab, e := datasets.BuildLabelVocabularyFromTgz(trainFn)
+	if e != nil {
+		log.Fatal(e)
+	}
+	log.Print("building label vocabulary done.")
+	model := models.Resnet50()
+	model.To(device)
+	model.Train(true)
+
 	lr := 0.1
 	momentum := 0.9
 	weightDecay := 1e-4
+	mbSize := 32
+	optimizer := torch.SGD(lr, momentum, 0, weightDecay, false)
+	optimizer.AddParameters(model.Parameters())
+
+	for epoch := 0; epoch < epochs; epoch++ {
+		adjustLearningRate(optimizer, epoch, lr)
+		startTime := time.Now()
+		trainLoader := imageNetLoader(trainFn, vocab, mbSize)
+		testLoader := imageNetLoader(testFn, vocab, mbSize)
+		iter := 0
+		samples := int64(0)
+		for trainLoader.Scan() {
+			iter++
+			data, label := trainLoader.Minibatch()
+			samples += data.Shape()[0]
+			optimizer.ZeroGrad()
+			loss, acc1, acc5 := trainOneMinibatch(data.To(device, data.Dtype()), label.To(device, label.Dtype()), model, optimizer)
+			if iter%logInterval == 0 {
+				log.Printf("Train Epoch: %d, Iteration: %d, loss:%f, acc1: %f, acc5:%f", epoch, iter, loss, acc1, acc5)
+			}
+		}
+		throughput := float64(samples) / time.Since(startTime).Seconds()
+		log.Printf("End Epoch: %d, throughput: %f samples/sec", epoch, throughput)
+		test(model, testLoader)
+	}
+	saveModel(model, save)
+}
+func saveModel(model *models.ResnetModule, modelFn string) {
+	log.Println("Saving model to", modelFn)
+	f, e := os.Create(modelFn)
+	if e != nil {
+		log.Fatalf("Cannot create file to save model: %v", e)
+	}
+	defer f.Close()
+
+	d := torch.NewDevice("cpu")
+	model.To(d)
+	if e := gob.NewEncoder(f).Encode(model.StateDict()); e != nil {
+		log.Fatal(e)
+	}
+}
+
+func predict(modelFn string, inputs []string) {
+	net := loadModel(modelFn)
+
+	for _, in := range inputs {
+		for _, pa := range strings.Split(in, ":") {
+			fns, e := filepath.Glob(pa)
+			if e != nil {
+				log.Fatal(e)
+			}
+
+			for _, fn := range fns {
+				predictFile(fn, net)
+			}
+		}
+	}
+}
+
+func loadModel(modelFn string) *models.ResnetModule {
+	f, e := os.Open(modelFn)
+	if e != nil {
+		log.Fatal(e)
+	}
+	defer f.Close()
+
+	states := make(map[string]torch.Tensor)
+	if e := gob.NewDecoder(f).Decode(&states); e != nil {
+		log.Fatal(e)
+	}
+
+	net := models.Resnet50()
+	net.SetStateDict(states)
+	return net
+}
+
+func predictFile(fn string, m *models.ResnetModule) {
+	f, e := os.Open(fn)
+	if e != nil {
+		log.Fatal(e)
+	}
+	defer f.Close()
+
+	img, _, e := image.Decode(f)
+	if e != nil {
+		log.Fatalf("Cannot decode input image: %v", e)
+	}
+	trans := transforms.Compose(
+		transforms.RandomResizedCrop(224),
+		transforms.RandomHorizontalFlip(0.5),
+		transforms.ToTensor(),
+		transforms.Normalize([]float64{0.485, 0.456, 0.406}, []float64{0.229, 0.224, 0.225}))
+	in := trans.Run(img)
+	fmt.Println(m.Forward(in.(torch.Tensor)).Argmax().Item())
+}
+
+func main() {
 	if torch.IsCUDAAvailable() {
 		log.Println("CUDA is valid")
 		device = torch.NewDevice("cuda")
@@ -118,65 +240,27 @@ func main() {
 		device = torch.NewDevice("cpu")
 	}
 
-	model := models.Resnet50()
-	model.To(device)
-	model.Train(true)
+	initializer.ManualSeed(1)
+	trainCmd := flag.NewFlagSet("train", flag.ExitOnError)
+	trainTar := trainCmd.String("data", "/tmp/imagenet_training_shuffled.tar.gz", "data tarball")
+	testTar := trainCmd.String("test", "/tmp/imagenet_testing_shuffled.tar.gz", "data tarball")
+	save := trainCmd.String("save", "/tmp/imagenet_model.gob", "the model file")
+	epochs := trainCmd.Int("epochs", 5, "the number of epochs")
 
-	optimizer := torch.SGD(lr, momentum, 0, weightDecay, false)
-	optimizer.AddParameters(model.Parameters())
-
-	f, e := os.Open(*trainFile)
-	if e != nil {
-		log.Fatal(e)
+	predictCmd := flag.NewFlagSet("predict", flag.ExitOnError)
+	load := predictCmd.String("load", "/tmp/mnist_model.gob", "the model file")
+	flag.Parse()
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s needs subcomamnd train or predict\n", os.Args[0])
+		os.Exit(1)
 	}
-	// build label vocabulary
-	vocab, e := datasets.BuildLabelVocabulary(f)
-	if e != nil {
-		log.Fatal(e)
-	}
-	log.Print("building label vocabulary done.")
 
-	iters := numIterPerEpoch(trainSamples, mbSize)
-	iter := 0
-	epoch := 0
-	adjustLearningRate(optimizer, epoch, lr)
-	skipSamples := 0
-	startTime := time.Now()
-	for epoch < epochs {
-		// seek to 0 of the file reader, and create an ImageNet loader
-		if _, e := f.Seek(0, io.SeekStart); e != nil {
-			log.Fatal(e)
-		}
-		loader, e := imageNetLoader(f, vocab, mbSize, skipSamples)
-		if e != nil {
-			panic(e)
-		}
-		for loader.Scan() {
-			iter++
-			image, target := loader.Minibatch()
-			optimizer.ZeroGrad()
-			loss, acc1, acc5 := trainOneMinibatch(image.To(device, torch.Float), target.To(device, torch.Long), model, optimizer)
-			if iter%logInterval == 0 {
-				throughput := float64(iter/logInterval) / time.Since(startTime).Seconds()
-				log.Printf("Epoch: %d, Iteration: %d, loss:%f, acc1: %f, acc5:%f, throughput: %f samples/secs",
-					epoch, iter, loss, acc1, acc5, throughput)
-				startTime = time.Now()
-			}
-			if iter == iters {
-				break
-			}
-		}
-		if iter == iters {
-			// go to next epoch
-			epoch++
-			adjustLearningRate(optimizer, epoch, lr)
-			iter = 0
-			skipSamples = rand.Intn(mbSize)
-			log.Printf("skip %d samples at the next epoch", skipSamples)
-		}
+	switch os.Args[1] {
+	case "train":
+		trainCmd.Parse(os.Args[2:])
+		train(*trainTar, *testTar, *save, *epochs)
+	case "predict":
+		predictCmd.Parse(os.Args[2:])
+		predict(*load, predictCmd.Args())
 	}
-}
-
-func numIterPerEpoch(samples, mbSize int) int {
-	return (trainSamples + mbSize - 1) / mbSize
 }
