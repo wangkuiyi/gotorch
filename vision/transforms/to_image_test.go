f8a76c967569bf2b91aa61bafec62b73e5be4ccf
Yi Wang <yi.wang.2005@gmail.com>
2020-09-05 17:07:09 -0700

    Add case

--- vision/transforms/to_image_test.go
+++ vision/transforms/to_image_test.go
@@ -104,19 +104,21 @@ func TestToImage(t *testing.T) {
 		assert.Equal(t, dark, im.At(1, 0))
 		assert.Equal(t, light, im.At(1, 1))
 	}
-	{ // case [n=1,1,h,w]
-		x := torch.NewTensor([][][][]int32{
-			{{{1, 1}, {0, 0}}}})
+	{ // case [n=1,3,h,w]
+		x := torch.NewTensor([][][][]int32{{
+			{{1, 0}, {0, 1}},
+			{{0, 1}, {0, 0}},
+			{{0, 0}, {1, 0}}}})
 		y := ToImage().Run(x)
 		assert.Equal(t, 1, len(y))
-		im, ok := y[0].(*image.Gray)
+		im, ok := y[0].(*image.RGBA)
 		assert.True(t, ok)
 		assert.NotNil(t, im)
 		assert.Equal(t, rect2x2, im.Bounds())
-		assert.Equal(t, light, im.At(0, 0))
-		assert.Equal(t, dark, im.At(0, 1))
-		assert.Equal(t, light, im.At(1, 0))
-		assert.Equal(t, dark, im.At(1, 1))
+		assert.Equal(t, red, im.At(0, 0))
+		assert.Equal(t, blue, im.At(0, 1))
+		assert.Equal(t, green, im.At(1, 0))
+		assert.Equal(t, red, im.At(1, 1))
 	}
 
 }
