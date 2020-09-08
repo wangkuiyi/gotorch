db4bab45bd2b7b88a49feefc8d826f47df8bdac9
Yi Wang <yi.wang.2005@gmail.com>
2020-09-08 15:49:20 -0700

    Add test case

--- tool/tgz/list_test.go
+++ tool/tgz/list_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/wangkuiyi/gotorch/tool/tgz"
 )
 
-func TestListFile(t *testing.T) {
+func TestTgzListFile(t *testing.T) {
 	d, e := ioutil.TempDir("", "gotorch_tarball_divide_test*")
 	if e != nil {
 		t.Fatal(e)
@@ -20,4 +20,10 @@ func TestListFile(t *testing.T) {
 	l, e := tgz.ListFile(fn)
 	assert.NoError(t, e)
 	assert.Equal(t, 5, len(l))
+}
+
+func TestTgzListNotExistingFile(t *testing.T) {
+	l, e := tgz.ListFile("somefile_not_there")
+	assert.Error(t, e)
+	assert.Nil(t, l)
 }
