e1d5d1681ab8318f30c5ab27777f31ad16593d39
Yi Wang <yi.wang.2005@gmail.com>
2020-09-08 15:47:11 -0700

    Update

--- tool/tgz/writer_test.go
+++ tool/tgz/writer_test.go
@@ -7,8 +7,13 @@ import (
 	"github.com/wangkuiyi/gotorch/tool/tgz"
 )
 
-func TestCreateFileNoPermission(t *testing.T) {
+func TestTgzCreateFileNoPermission(t *testing.T) {
 	w, e := tgz.CreateFile("/")
 	assert.Nil(t, w)
 	assert.Error(t, e)
+}
+
+func TestTgzNewWriterFromNil(t *testing.T) {
+	w := tgz.NewWriter(nil)
+	assert.Nil(t, w)
 }
