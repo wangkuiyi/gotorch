db6e27b772ac9886ae297d0e40380a717acfc368
qijun <qijun1994@hotmail.com>
2020-09-08 18:52:30 +0800

    switch to deep copy in FromBlob method

--- cgotorch/tensor.cc
+++ cgotorch/tensor.cc
@@ -165,7 +165,8 @@ const char *Tensor_FromBlob(void *data, int8_t dtype, int64_t *sizes_data,
                             int64_t sizes_data_len, Tensor *result) {
   try {
     auto t = at::from_blob(data, at::IntArrayRef(sizes_data, sizes_data_len),
-                           torch::dtype(at::ScalarType(dtype)));
+                           torch::dtype(at::ScalarType(dtype)))
+                 .clone();
     *result = new at::Tensor(t);
     return nullptr;
   } catch (const std::exception &e) {
