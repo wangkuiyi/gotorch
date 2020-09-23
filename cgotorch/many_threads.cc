cb197f4a6d0bd9a1719799fe426896ed8aad4545
shendiaomo <shendiaomo@gmail.com>
2020-09-23 23:00:05 +0800

    Locate the root cause more precisely

--- cgotorch/many_threads.cc
+++ cgotorch/many_threads.cc
@@ -1,22 +1,23 @@
-#include <stdlib.h>
+#include <omp.h>
 
+#include <cassert>
 #include <chrono>
+#include <cstdlib>
 #include <iostream>
+#include <mutex>
 #include <sstream>
 #include <string>
 #include <thread>
+#include <vector>
 
-#include "torch/torch.h"
-
-namespace nn = torch::nn;  // for the literal `ms`
-
-using namespace std::chrono_literals;
+using namespace std::chrono_literals;  // for the literal `s` and `ms`
 
 std::mutex mu;
 
 int main(int argc, char* argv[]) {
   std::string argv0 = argv[0];
-  if (auto pos = argv0.rfind('/'); pos != std::string::npos) {
+  auto pos = argv0.rfind('/');
+  if (pos != std::string::npos) {
     argv0 = argv0.substr(pos + 1);
   }
   std::stringstream thread_count_command;
@@ -26,17 +27,6 @@ int main(int argc, char* argv[]) {
   std::cout << std::string(20, '-') << std::endl;
 
   std::vector<std::thread> pool;
-  // Prepare inputs
-  auto x = torch::arange(75, torch::dtype(torch::kFloat).requires_grad(false))
-               .reshape({1, 3, 5, 5});
-  auto weight =
-      torch::arange(54, torch::dtype(torch::kFloat).requires_grad(false))
-          .reshape({2, 3, 3, 3});
-  auto kernel_size = weight.sizes().slice(2);
-  auto output = at::empty({0}, x.options());
-  auto finput = at::empty({0}, x.options());
-  auto fgrad_input = at::empty({0}, x.options());
-
   auto total = std::thread::hardware_concurrency();
   if (argc > 1) total = std::atoi(argv[1]);
 
@@ -50,15 +40,19 @@ int main(int argc, char* argv[]) {
           std::cout << "Thread " << i << "(" << std::this_thread::get_id()
                     << "), step " << step << std::endl;
           std::cout << "#Threads before `forward`:" << std::endl;
-          auto _ = system(thread_count_command.str().c_str());
-          at::native::slow_conv2d_forward_out_cpu(output, finput, fgrad_input,
-                                                  x, weight, kernel_size, {},
-                                                  {1, 1}, {0, 0});
+          assert(std::system(thread_count_command.str().c_str()) == 0);
+
+          // The following OMP code is from `at::parallel_for`
+          // There's a similar problem in
+          // https://github.com/pytorch/pytorch/issues/32008
+#pragma omp parallel if (omp_get_max_threads() > 1 && !omp_in_parallel())
+          omp_get_num_threads();
+
           std::cout << "#Threads after `forward`:" << std::endl;
-          _ = system(thread_count_command.str().c_str());
+          assert(std::system(thread_count_command.str().c_str()) == 0);
           std::cout << std::string(20, '-') << std::endl;
         }
-        std::this_thread::sleep_for(2s);  // Yield to another thread
+        std::this_thread::sleep_for(1s);  // Yield to another thread
       }
     }));
   }
